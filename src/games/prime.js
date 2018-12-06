5d1e6b73320375069bf04242effb3472bf185e9b
Dael777 <dael85@gmail.com>
2018-12-06 18:11:57 +0300

    step 8 fixes

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,18 +5,23 @@ import game from '..';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const minNumber = 1;
 const maxNumber = 1000;
-const minPossibleDivisor = 2;
 
-const isPrime = (num, divisor) => {
-  if (divisor >= num / 2) return true;
-  if (num % divisor === 0) return false;
+const isPrime = (number) => {
+  const minPossibleDivisor = 2;
 
-  return isPrime(num, divisor + 1);
+  const iter = (num, divisor) => {
+    if (divisor >= num / 2) return true;
+    if (num % divisor === 0) return false;
+
+    return iter(num, divisor + 1);
+  };
+
+  return iter(number, minPossibleDivisor);
 };
 
 const makeQuiz = () => {
   const question = generateNumber(minNumber, maxNumber);
-  const correctAnswer = isPrime(question, minPossibleDivisor) ? 'yes' : 'no';
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
 
   return cons(question, correctAnswer);
 };
